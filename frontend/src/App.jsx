4dce27b39fa44abec185e3336bd80812051ab432
Neel Sathvara <sathvaraneel52@gmail.com>
2025-01-17 17:07:14 +0530

    unupdate app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,9 +33,6 @@ function App() {
                                   <Route path='/signup' element={<SignUp/>}></Route>
                                   <Route path='/all-doctors' element={<Doctors/>}></Route>
                                   <Route path='/admin/*' element={<Admin/>}></Route>
-                                  <Route path='/app' element={<>
-                                  <h1>Hello</h1>
-                                  </>}></Route>
                              </Routes>
                              </div>
                        </div>
