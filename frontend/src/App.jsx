56b2973fd8a9d3fa586f90f75bfa39db45f72655
Darshan Kharva <darshan.kharva11@gmail.com>
2025-01-18 15:53:59 +0530

    leraning 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,7 @@ function App() {
                                   <Route path='/signup' element={<SignUp/>}></Route>
                                   <Route path='/all-doctors' element={<Doctors/>}></Route>
                                   <Route path='/admin/*' element={<Admin/>}></Route>
+                                  <Route path='/learn' element={<><h1>Learn</h1></>}></Route>
                              </Routes>
                              </div>
                        </div>
