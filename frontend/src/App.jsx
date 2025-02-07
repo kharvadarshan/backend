c3a96994495af6ed1202e9cab0bc3b63c15deb35
Neel Sathvara <sathvaraneel52@gmail.com>
2025-02-07 16:50:36 +0530

    "doctor"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,16 @@
-
-<<<<<<< HEAD
-=======
-
->>>>>>> ef3e23e549a9632ceb7a3fb5e0ffdb81ea4b6d52
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Route,Routes } from 'react-router-dom';
 import Login from "../src/components/Login";
 import SignUp from "../src/components/SignUp";
-<<<<<<< HEAD
 // import Doctors from "../src/components/Doctors";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Admin from './components/Admin';
-=======
 //import Doctors from "./components/Doctor";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import Admin from './components/Admin';
 import Contact from './components/Contact';
 // import NavBar from './components/NavBar';
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -34,18 +24,12 @@ import Contact from './components/Contact';
 // import Footer from './components/Footer';
 // import Admin from './components/Admin';
 
->>>>>>> ef3e23e549a9632ceb7a3fb5e0ffdb81ea4b6d52
 // import ContactApp from './components/Contact/contactApp';
 
 import Doctor from "./components/Doctor";
 
 function App() {
     return(
-<<<<<<< HEAD
-=======
-    
-          
->>>>>>> ef3e23e549a9632ceb7a3fb5e0ffdb81ea4b6d52
         <>
           <div className='container min-vw-100 min-vh-100'>
            <div className='row g-0 custom-row'>
@@ -64,10 +48,9 @@ function App() {
                                   }></Route>
                                   <Route path='/login' element={<Login/>}></Route>
                                   <Route path='/signup' element={<SignUp/>}></Route>
-                                  <Route path='/all-doctors' element={ <Doctor_List/>}></Route>
+                                  <Route path='/all-doctors' element={ <Doctor/>}></Route>
                                   <Route path='/admin/*' element={<Admin/>}></Route>
                                   <Route path='/contact' element={<Contact/>}></Route>
->>>>>>> ef3e23e549a9632ceb7a3fb5e0ffdb81ea4b6d52
                                   
                              </Routes>
                              </div>
@@ -78,11 +61,7 @@ function App() {
            </div>
           </div>
             
-        </>
-<<<<<<< HEAD
-=======
-       
->>>>>>> ef3e23e549a9632ceb7a3fb5e0ffdb81ea4b6d52
+        </>       
     )
 }
 
