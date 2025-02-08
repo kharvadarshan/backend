6737b29f1e84a384ade8202c2f5d75058edda9e7
Neel Sathvara <sathvaraneel52@gmail.com>
2025-02-09 00:48:05 +0530

    "neel_branch_update"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,14 @@
-import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Route,Routes } from 'react-router-dom';
-import Login from "../src/components/Login";
-import SignUp from "../src/components/SignUp";
-// import Doctors from "../src/components/Doctors";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import Admin from './components/Admin';
-//import Doctors from "./components/Doctor";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Contact from './components/Contact';
-// import NavBar from './components/NavBar';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import { Route,Routes } from 'react-router-dom';
-// import Login from "../src/components/Login";
-// import SignUp from "../src/components/SignUp";
-// import Doctors from "../src/components/Doctors";
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-// import Home from './components/Home';
-// import Footer from './components/Footer';
-// import Admin from './components/Admin';
-
-// import ContactApp from './components/Contact/contactApp';
-
-import Doctor from "./components/Doctor";
-
-function App() {
-    return(
 import  AllRoute from '../src/routes';
 
+
 function App() {
     return(
-
         <>
-          <AllRoute/>
-            
+          <AllRoute/> 
         </>       
     )
 }
