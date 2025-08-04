68a65f4573568c37160eba875074c3b615dc5e00
kharvadarshan <150101961+kharvadarshan@users.noreply.github.com>
2025-08-04 10:36:12 +0530

    chnage in api url of logout

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
 
   const logout = async () => {
     try {
-      const result = await axios.post(
+      const result = await axios.get(
         `${import.meta.env.VITE_API_URL}/user/logout`,
         null,
         {
