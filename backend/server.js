2037d757443bfc5cd5e996e37477823c16536f03
kharvadarshan <150101961+kharvadarshan@users.noreply.github.com>
2025-08-04 12:05:05 +0530

    cors change in setcookie

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,8 @@ app.use(
     origin:["http://localhost:5173","http://localhost:5174","https://bookmydoctor-frontend-g4pe.onrender.com"], 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization','Set-Cookie'],
+    exposedHeaders: ['Set-Cookie']
 }));
 app.use(express.json()); // For parsing JSON
 app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded form data
